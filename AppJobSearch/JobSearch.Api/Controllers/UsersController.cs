6f2b0cadc7a18a222bd18afbc3f3f9b6b28e2d7a
Luiz Chequini <luizchequini@hotmail.com>
2020-09-15 01:04:15 -0300

    Criei a UserController com os métodos de Login GetUser HttpGet e Adicionar usuário AddUser HttpPost

---
+++ AppJobSearch/JobSearch.Api/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using JobSearch.Api.Database;
+using JobSearch.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace JobSearch.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private JobSearchContext _jobSearchContext;
+
+        public UsersController(JobSearchContext jobSearchContext)
+        {
+            _jobSearchContext = jobSearchContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetUser(string email, string password)
+        {
+            var userDb = _jobSearchContext.Users.FirstOrDefault(u => u.Email.Equals(email) && u.Password.Equals(password));
+
+            if (userDb == null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(userDb);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            _jobSearchContext.Add(user);
+            _jobSearchContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetUser), new { email = user.Email, password = user.Password }, user);
+        }
+    }
+}
