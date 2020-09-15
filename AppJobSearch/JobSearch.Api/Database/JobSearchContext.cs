369f3505bf02043cb157d10c7146fadb75d07f7e
Luiz Chequini <luizchequini@hotmail.com>
2020-09-15 00:35:26 -0300

    Executei a Migração criando o BancoInicial e com a execução do update-database foi criado o Banco de Dados Sqlite JobSearch.db

--- AppJobSearch/JobSearch.Api/Database/JobSearchContext.cs
+++ AppJobSearch/JobSearch.Api/Database/JobSearchContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using JobSearch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobSearch.Api.Database
 {
     public class JobSearchContext : DbContext
     {
+        public JobSearchContext(DbContextOptions<JobSearchContext> options):base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Job> Jobs { get; set; }
     }
 }
