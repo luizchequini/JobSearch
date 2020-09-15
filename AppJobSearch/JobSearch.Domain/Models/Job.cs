ae0b68d539b4c81cae7170a634522cb7ca1cc54e
Luiz Chequini <luizchequini@hotmail.com>
2020-09-15 00:01:22 -0300

    Criação dos Modelos juntamente com seus atributos sendo o User e Job

---
+++ AppJobSearch/JobSearch.Domain/Models/Job.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobSearch.Domain.Models
+{
+    public class Job
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string Company { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string JobTitle { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string CityState { get; set; }
+
+        [Required]
+        public double Salary { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string ContractType { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string TecnologyTools { get; set; }
+
+        public string CompanyDescription { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        public string JobDescription { get; set; }
+
+        public string Benefits { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string InterestedSendEmailTo { get; set; }
+
+
+        public DateTime PublicationDate { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+
+
+
+    }
+}
---
+++ AppJobSearch/JobSearch.Domain/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobSearch.Domain.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
