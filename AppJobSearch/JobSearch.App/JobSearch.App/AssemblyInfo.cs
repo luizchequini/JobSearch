53ee7b8046cca9ce48c96345a2a7e5842cb2935c
Luiz Chequini <luizchequini@hotmail.com>
2020-09-18 11:35:50 -0300

    Dentro de cada projeto Xamarin Forms, foi inserido  todos os icones nas pastas Criei a tela de pesquisa  com Content Page e dei o nome de Start e criei todos os componentes da Tela XAML

--- AppJobSearch/JobSearch.App/JobSearch.App/AssemblyInfo.cs
+++ AppJobSearch/JobSearch.App/JobSearch.App/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "MontserratRegular")]
 [assembly: ExportFont("Montserrat-Light.ttf", Alias = "MontserratLight")]
 [assembly: ExportFont("Montserrat-Bold.ttf", Alias = "MontserratBold")]
