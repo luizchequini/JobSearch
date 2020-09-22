1d159758ebff1ce4200a3b77c09d9b3cac9aefc5
Luiz Chequini <luizchequini@hotmail.com>
2020-09-22 00:06:15 -0300

    Criação dos Serviços de manutenção de Users e Jobs

---
+++ AppJobSearch/JobSearch.App/JobSearch.App/Services/JobService.cs
@@ -0,0 +1,57 @@
+﻿using JobSearch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearch.App.Services
+{
+    public class JobService : Service
+    {
+        public async Task<IEnumerable<Job>> GetJobs(string word, string citystate, int numberofpage = 1)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"{_baseApiUrl}/api/jobs?word={word}&citystate={citystate}&numberofpage={numberofpage}");
+
+            List<Job> list = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                list = await response.Content.ReadAsAsync<List<Job>>();
+            }
+
+            return list;
+
+        }
+
+        public async Task<Job> GetJob(int id)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"{_baseApiUrl}/api/Jobs/{id}");
+
+            Job job = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                job = await response.Content.ReadAsAsync<Job>();
+            }
+
+            return job;
+        }
+
+        public async Task<Job> AddJob(Job job)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync($"{_baseApiUrl}/api/jobs/", job);
+
+            if (response.IsSuccessStatusCode)
+            {
+                job = await response.Content.ReadAsAsync<Job>();
+            }
+            else
+            {
+                job = null;
+            }
+
+            return job;
+        }
+    }
+}
---
+++ AppJobSearch/JobSearch.App/JobSearch.App/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using JobSearch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSearch.App.Services
+{
+    public class UserService : Service
+    {
+        public async Task<User> GetUser(string email, string password)
+        {
+            HttpResponseMessage response =  await _httpClient.GetAsync($"{_baseApiUrl}/api/Users?email={email}&password={password}");
+
+            User user = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                user = await response.Content.ReadAsAsync<User>();
+            }
+
+            return user;
+        }
+
+        public async Task<User> AddUser(User user)
+        {
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync($"{_baseApiUrl}/api/users", user);
+
+            if (response.IsSuccessStatusCode)
+            {
+                user = await response.Content.ReadAsAsync<User>();
+            }
+            else
+            {
+                user = null;
+            }
+
+            return user;
+        }
+    }
+}
